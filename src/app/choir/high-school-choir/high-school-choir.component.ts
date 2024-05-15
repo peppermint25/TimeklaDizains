c0cf02eb961f395519d75b332d6edc0100ed1460
peppermint25 <oskars.metr01@gmail.com>
2024-05-15 17:51:22 +0300

    small fix

--- src/app/choir/high-school-choir/high-school-choir.component.ts
+++ src/app/choir/high-school-choir/high-school-choir.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-high-school-choir',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './high-school-choir.component.html',
   styleUrl: './high-school-choir.component.scss'
 })
