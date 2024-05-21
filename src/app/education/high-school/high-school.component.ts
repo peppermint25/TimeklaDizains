c1dabd33bdd4804a21808b353bd29113992c91ed
peppermint25 <oskars.metr01@gmail.com>
2024-05-21 18:36:15 +0300

    more data

--- src/app/education/high-school/high-school.component.ts
+++ src/app/education/high-school/high-school.component.ts
@@ -1,9 +1,10 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-high-school',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './high-school.component.html',
   styleUrl: './high-school.component.scss'
 })
