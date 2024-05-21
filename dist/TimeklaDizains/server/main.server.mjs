4fa805f76b2d4e13aed1465e19be4dc1d3d521c3
peppermint25 <oskars.metr01@gmail.com>
2024-05-21 23:53:00 +0300

    more info

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QSFFVYL6.mjs";import"./chunk-KTHRU3RA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GRAPFLUK.mjs";import"./chunk-KTHRU3RA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/education/primary-school/primary-school.component.ts
+++ src/app/education/primary-school/primary-school.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-primary-school',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './primary-school.component.html',
   styleUrl: './primary-school.component.scss'
 })
