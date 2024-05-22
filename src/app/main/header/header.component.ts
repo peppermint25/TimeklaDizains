c908388a6eb59dd5bd8d8e86eeda85e64cb760be
peppermint25 <oskars.metr01@gmail.com>
2024-05-22 14:34:32 +0300

    maybe?

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +10,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private router: Router){
+    console.log(router.url)
+  }
+
+
+  ngOnInit(): void {
+      
+  }
 }
