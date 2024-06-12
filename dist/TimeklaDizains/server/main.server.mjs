c9d5253f8240604faacc77ac0c4d3707e7951b2f
peppermint25 <oskars.metr01@gmail.com>
2024-06-12 23:32:53 +0300

    hmmmmmm

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7KOAYAFU.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-BFXXBYHL.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/education/education-main/education-main.component.ts
+++ src/app/education/education-main/education-main.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+
 import { HeaderComponent } from '../header/header.component';
-import {RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy, Renderer2, Inject, ViewChild, ElementRef } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-education-main',
@@ -10,5 +14,37 @@ import {RouterOutlet } from '@angular/router';
   styleUrl: './education-main.component.scss'
 })
 export class EducationMainComponent {
+  private themeSubscription: Subscription | undefined;
 
+  constructor(
+    private themeService: ThemeService,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngOnInit(): void {
+    console.log('ChoirMainComponent initialized');
+    this.themeSubscription = this.themeService.theme$.subscribe(theme => {
+      console.log('ChoirMainComponent theme subscription');
+      this.applyTheme(theme);
+    });
+
+    // Apply initial theme
+    this.applyTheme(localStorage.getItem('theme'));
+  }
+
+  ngOnDestroy(): void {
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
+
+  private applyTheme(theme: string | null): void {
+    const body = this.document.querySelector('div.sub-body');
+    if (theme === 'dark') {
+      this.renderer.addClass(body, 'dark');
+    } else {
+      this.renderer.removeClass(body, 'dark');
+    }
+  }
 }
