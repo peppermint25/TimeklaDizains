e77079c5e8b157491d9dcc790981887de8f4edc3
peppermint25 <oskars.metr01@gmail.com>
2024-06-12 13:31:25 +0300

    test

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7QB5J5KO.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-7L7DBHBN.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/choir/choir-main/choir-main.component.ts
+++ src/app/choir/choir-main/choir-main.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2, Inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 import { ChoirHeaderComponent } from '../choir-header/choir-header.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,8 +13,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './choir-main.component.scss'
 })
 export class ChoirMainComponent implements OnInit{
+  private themeSubscription: Subscription | undefined;
+
+  constructor(
+    private themeService: ThemeService,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   ngOnInit(): void {
-      localStorage.getItem('theme') === 'dark' ? document.body.classList.add('dark') : document.body.classList.remove('dark');
+    console.log('ChoirHeaderComponent initialized');
+    this.themeSubscription = this.themeService.theme$.subscribe(theme => {
+      console.log('ChoirHeaderComponent theme subscription');
+      this.applyTheme(theme);
+    });
+
+    // Apply initial theme
+    this.applyTheme(localStorage.getItem('theme'));
+  }
+
+  ngOnDestroy(): void {
+    if (this.themeSubscription) {
+      this.themeSubscription.unsubscribe();
+    }
+  }
+
+  private applyTheme(theme: string | null): void {
+    if (theme === 'dark') {
+      this.renderer.addClass(this.document.body, 'dark');
+    } else {
+      this.renderer.removeClass(this.document.body, 'dark');
+    }
   }
 }
