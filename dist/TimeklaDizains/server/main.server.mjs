6b245ae7a002258fb2116df7b211a62563fe3fc9
peppermint25 <oskars.metr01@gmail.com>
2024-06-13 08:55:28 +0300

    fix?

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DKVBV6EZ.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-M7IJHZVT.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
