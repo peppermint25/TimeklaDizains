8dca4e28fab67bdd9606c9f73d70485cd5e10a3f
peppermint25 <oskars.metr01@gmail.com>
2024-06-13 09:12:22 +0300

    should be

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M7IJHZVT.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-J5JYN5LU.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
