54d871bcbf22419b4cc56896b7efecaf001545f8
peppermint25 <oskars.metr01@gmail.com>
2024-06-13 09:25:39 +0300

    small update

--- dist/TimeklaDizains/server/main.server.mjs
+++ dist/TimeklaDizains/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-J5JYN5LU.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-YG6OY6Y7.mjs";import"./chunk-AAHJ6BMB.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
